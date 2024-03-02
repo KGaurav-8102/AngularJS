6736f49f85a8d82ad32be07456569478301bfa74
Gaurav Yadav <gk5480325@gmail.com>
2024-03-02 10:42:39 +0530

    hero -child and hero-parent comp data interaction initialzed

---
+++ src/app/hero-child/hero-child.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-child',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-child.component.html',
+  styleUrl: './hero-child.component.css'
+})
+export class HeroChildComponent {
+
+}
---
+++ src/app/hero-parent/hero-parent.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-hero-parent',
+  standalone: true,
+  imports: [],
+  templateUrl: './hero-parent.component.html',
+  styleUrl: './hero-parent.component.css'
+})
+export class HeroParentComponent {
+
+}
